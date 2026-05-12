6086a9d1727b3d7e29bbc2bfa3059e93f7285009
rorygoodman <rorydpg@gmail.com>
2026-05-13 00:28:43 +0100

    Drop Selenium and slf4j-simple from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.seleniumhq.selenium:selenium-java:4.16.1")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("com.google.code.gson:gson:2.10.1")
 
     testImplementation(kotlin("test"))
